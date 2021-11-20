71a8f906dd8360a6952ddd7ee103bc068a371888
Juna Salviati <antigones@users.noreply.github.com>
2021-11-20 01:24:30 +0100

    flaked

--- l_system.py
+++ l_system.py
@@ -1,7 +1,8 @@
 from turtle import *
 
+
 class LSystem:
-    
+
     def walk_F(self):
         return forward(self.step_size)
 
@@ -12,7 +13,7 @@ class LSystem:
         return right(self.angle)
 
     def push_pos_angle(self):
-        return self.stack.append((pos(),heading()))
+        return self.stack.append((pos(), heading()))
 
     def pop_pos_angle(self):
         pos, heading = self.stack.pop()
@@ -22,11 +23,11 @@ class LSystem:
         pendown()
 
     def push_pos_set_angle(self):
-        self.stack.append((pos(),heading()))
+        self.stack.append((pos(), heading()))
         setheading(45)
-    
+
     def pop_pos_set_angle(self):
-        pos, _ =  self.stack.pop()
+        pos, _ = self.stack.pop()
         penup()
         setposition(pos)
         setheading(-45)
@@ -42,18 +43,18 @@ class LSystem:
 
     def print_pattern(self):
         draw_pattern = {
-            "F":self.walk_F,
-            "+":self.walk_L,
-            "-":self.walk_R,
-            "[":self.push_pos_angle,
-            "]":self.pop_pos_angle,
-            "A":self.walk_F,
-            "B":self.move_forward,
-            "G":self.walk_F,
-            "0":self.draw_tip,
-            "1":self.walk_F,
-            "{":self.push_pos_set_angle,
-            "}":self.pop_pos_set_angle,
+            "F": self.walk_F,
+            "+": self.walk_L,
+            "-": self.walk_R,
+            "[": self.push_pos_angle,
+            "]": self.pop_pos_angle,
+            "A": self.walk_F,
+            "B": self.move_forward,
+            "G": self.walk_F,
+            "0": self.draw_tip,
+            "1": self.walk_F,
+            "{": self.push_pos_set_angle,
+            "}": self.pop_pos_set_angle,
         }
         speed(10)
         pensize(self.line_thickness)
@@ -61,16 +62,16 @@ class LSystem:
         begin_fill()
         for chr in self.end_pattern:
             if chr in draw_pattern:
-                draw_pattern[chr]()   
+                draw_pattern[chr]()
         end_fill()
         done()
 
-    def expand(self, starting_pattern:str, times:int):
+    def expand(self, starting_pattern: str, times: int):
         for i in range(times):
             end_pattern = ""
             for chr in starting_pattern:
                 end_pattern += self.expansion_rules[chr]
-            starting_pattern = end_pattern           
+            starting_pattern = end_pattern
         return starting_pattern
 
     def __init__(self, omega, expansion_rules, angle, times):
@@ -81,4 +82,4 @@ class LSystem:
         self.angle = angle
         self.times = times
         self.stack = []
-        self.end_pattern = self.expand(self.omega,times)
+        self.end_pattern = self.expand(self.omega, times)
