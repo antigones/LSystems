ef0cbf69059c7d23594866b3c8e2538a2cecf8d6
Juna Salviati <antigones@users.noreply.github.com>
2021-11-20 01:27:31 +0100

    source flaked

--- l_system_test.py
+++ l_system_test.py
@@ -1,84 +1,92 @@
 from l_system import LSystem
 
+
 def draw_peano():
     peano_omega = "L"
     peano_expansion_rules = {
                 "L": "LFRFL-F-RFLFR+F+LFRFL",
                 "R": "RFLFR+F+LFRFL-F-RFLFR",
-                "F":"F",
-                "+":"+",
-                "-":"-"
+                "F": "F",
+                "+": "+",
+                "-": "-"
             }
-    peano = LSystem(peano_omega,peano_expansion_rules,90,2)
+    peano = LSystem(peano_omega, peano_expansion_rules, 90, 2)
     peano.print_pattern()
+
 
 def draw_hilbert():
     hilber_omega = "L"
     hilbert_expansion_rules = {
-        "L":"+RF-LFL-FR+",
-        "R":"-LF+RFR+FL-",
-        "F":"F",
-        "+":"+",
-        "-":"-"
+        "L": "+RF-LFL-FR+",
+        "R": "-LF+RFR+FL-",
+        "F": "F",
+        "+": "+",
+        "-": "-"
     }
 
     hilbert = LSystem(hilber_omega, hilbert_expansion_rules, 90, 3)
     hilbert.print_pattern()
 
+
 def draw_koch():
     koch_omega = "F"
     koch_expansion_rules = {
-        "F":"F+F-F-F+F",
-        "+":"+",
-        "-":"-"
+        "F": "F+F-F-F+F",
+        "+": "+",
+        "-": "-"
     }
 
     koch = LSystem(koch_omega, koch_expansion_rules, 90, 3)
     koch.print_pattern()
 
+
 def draw_fractal_plant():
     fractal_plant_omega = "X"
     fractal_plant_expansion_rules = {
-        "X":"F+[[X]-X]-F[-FX]+X",
-        "F":"FF",
-        "[":"[",
-        "]":"]",
-        "+":"+",
-        "-":"-"
+        "X": "F+[[X]-X]-F[-FX]+X",
+        "F": "FF",
+        "[": "[",
+        "]": "]",
+        "+": "+",
+        "-": "-"
     }
     fractal_plant = LSystem(fractal_plant_omega, fractal_plant_expansion_rules, 25, 4)
-    fractal_plant.print_pattern()    
+    fractal_plant.print_pattern()
+
 
 def draw_cantor():
     cantor_omega = "A"
     cantor_expansion_rules = {
-        "A":"ABA",
-        "B":"BBB",
+        "A": "ABA",
+        "B": "BBB",
     }
     cantor_set = LSystem(cantor_omega, cantor_expansion_rules, 0, 2)
     cantor_set.print_pattern()
 
+
 def draw_sierpinsky():
     sierpinsky_omega = "F-G-G"
     sierpinsky_expansion_rules = {
-        "F":"F-G+F+G-F",
-        "G":"GG",
-        "+":"+",
-        "-":"-"
+        "F": "F-G+F+G-F",
+        "G": "GG",
+        "+": "+",
+        "-": "-"
     }
     sierpinsky = LSystem(sierpinsky_omega, sierpinsky_expansion_rules, 120, 3)
     sierpinsky.print_pattern()
 
+
 def draw_fractal_tree():
     fractal_tree_omega = "0"
     fractal_tree_expansion_rules = {
-        "1":"11",
-        "0":"1{0}0",
-        "{":"{",
-        "}":"}",
+        "1": "11",
+        "0": "1{0}0",
+        "{": "{",
+        "}": "}",
     }
     fractal_tree = LSystem(fractal_tree_omega, fractal_tree_expansion_rules, 45, 3)
     fractal_tree.print_pattern()
+
 
 def main():
     # draw_fractal_tree()
